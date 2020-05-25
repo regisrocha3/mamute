0eb61779c80d9b88c90eb187414740b3efebdc2e
regisrocha3 <regisrocha3@gmail.com>
2020-05-25 01:02:14 -0300

    feat: MAM-54-Listagem-de-Usuarios

--- src/main/java/br/com/caelum/mamute/user/domain/SearchCriteria.java
+++ src/main/java/br/com/caelum/mamute/user/domain/SearchCriteria.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.domain.Specification;
 
 public interface SearchCriteria {
 
-    Specification<UserEntity> toSpecification();
+    Specification<?> toSpecification();
 
 }
